0379b36033cadac9e7e5a68dbb8d2642ad711de7
peach909 <99386746+peach909@users.noreply.github.com>
2022-11-28 17:46:02 +0300

    Delete individyalnoe zadanie 5.py

--- individyalnoe zadanie 5.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    k=0
-    with open ("prosto text.txt","r", encoding="utf-8") as fileptr:
-        sentences =f.readlines()
-        for sentence in sentences:
-            if "друг" in sentence:
-                 k=k+1
-                 print(k)
